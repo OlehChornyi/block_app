e201b685ca46c325fbfda743b3e516c09ad57066
oleh <phd.chornyi@gmail.com>
2024-10-08 17:32:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Bloc Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+      
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      
+        title: Text(widget.title),
+      ),
+      body: Center(
+    
+        child: Column(
+      
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Navigate to one of the following screens',
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Counter Screen')),
+            ElevatedButton(onPressed: () {}, child: Text('API Screen')),
+           
+          ],
+        ),
+      ),
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
